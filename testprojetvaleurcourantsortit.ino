80de8fc9a468dc929db4f7392fc60b59e921bc18
glav56 <63360781+glav56@users.noreply.github.com>
2023-05-26 09:34:41 +0200

    Add files via upload

--- testprojetvaleurcourantsortit.ino
+++ testprojetvaleurcourantsortit.ino
@@ -5,6 +5,13 @@
     #define DENSITY_WATER 1  // Pure water density normalized to 1
     #define DENSITY_GASOLINE 0.74  // Gasoline density
     #define PRINT_INTERVAL 1000
+    #define debug 
+    #define SENSOR          3
+    #define DEBOUNCE_TIME   100
+    
+   float precipitation = 0;
+   volatile int pulse = 0;
+   static int last_pulse = 0;
 
     int16_t dataVoltage;
     float dataCurrent, depth; //unit:mA
@@ -15,10 +22,17 @@
       Serial.begin(9600);
       pinMode(ANALOG_PIN, INPUT);
       timepoint_measure = millis();
+       pinMode(SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SENSOR), isr, CHANGE);
     }
 
     void loop()
     {
+      int actual_pulse;
+  noInterrupts();
+  
+  actual_pulse = pulse;
+  interrupts();
       if (millis() - timepoint_measure > PRINT_INTERVAL) {
         timepoint_measure = millis();
 
@@ -30,6 +44,12 @@
         {
           depth = 0.0;
         }
+        if (last_pulse != actual_pulse) {
+
+        Serial.println((float) actual_pulse / 100);
+
+        last_pulse = actual_pulse;
+        }
 
         //Serial print results
         Serial.print("depth:");
@@ -37,3 +57,24 @@
         Serial.println("mm");
       }
     }
+
+    int pinState, previousPinState = HIGH;
+
+    void isr()
+{
+  static unsigned long last_interrupt_time;
+  unsigned long interrupt_time = millis();
+
+  pinState = digitalRead(SENSOR);
+  if (pinState != previousPinState) {
+    if (pinState == LOW) {
+      if (interrupt_time - last_interrupt_time >= DEBOUNCE_TIME) {
+        pulse += 0.28 * 100;
+        
+        
+      }
+    }
+    last_interrupt_time = interrupt_time;
+    previousPinState = pinState;
+  }
+}
